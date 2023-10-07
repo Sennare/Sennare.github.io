9008eb215d2e2e851f813e41c0a5d6073c9489f0
Elia Rebellato <reb.sennare@gmail.com>
2023-10-07 14:14:08 +0200

    Fix UI and calculate operation

--- src/components/my-core.ts
+++ src/components/my-core.ts
@@ -1,55 +1,140 @@
-import { LitElement, html } from 'lit';
+import { LitElement, PropertyValueMap, css, html } from 'lit';
 import { customElement, state, query } from 'lit/decorators.js';
 
+interface TeamMember {
+  name: string;
+  weight: number;
+}
 
 @customElement('my-core')
 export class MyCore extends LitElement {
 
-  @query('#add-member-team1')
-  input!: HTMLInputElement;
+  static styles = css`
+    .container {
+      width: 100%;
+      max-width: 500px;
+      margin: 0 auto;
+    }
+
+    .add-form {
+      width: 100%;
+      display: grid;
+      grid-template-columns: auto minmax(10px, 1fr);
+      grid-gap: 4px 12px;
+      margin: 10px 0;
+    }
+
+    .add-form .full-row {
+      grid-column-start: 1;
+      grid-column-end: 3;
+      display: inline-grid;
+    }
+
+    .member {
+      width: 100%;
+      display: grid;
+      grid-template-columns: auto minmax(10px, 1fr) 30px;
+      grid-gap: 4px 12px;
+      margin: 10px 0;
+    }
+
+    .member.non-deletable {
+      grid-template-columns: auto minmax(10px, 1fr);
+    }
+
+    .bold {
+      font-weight: 600;
+    }
+
+    .clickable {
+      cursor: pointer;
+    }
+  `;
+
+  @query('form')
+  $form: HTMLFormElement;
 
   @state()
-  private team1: string[] = [];
+  private team1: TeamMember[] = [];
 
   @state()
-  private team2: string[] = [];
+  private solution: TeamMember[] = [];
 
   render() {
-    const team1Joined = this.team1.join(", ");
-    console.log("render with list" + team1Joined);
     return html`
-    <p>Benvenuto al'organizzatore per il tiro alla fune!</p>
-    <p>Aggiungi un giocatore:</p>
-    <input type="text" class="form-control" id="add-member-team1"> <br />
-    <button type="submit" class="btn btn-primary" @click=${this.addPlayer}>Aggiungi</button> <br />
-    <p>${team1Joined}<p/>
+    <div class="container">
+      <p>Benvenuto al'organizzatore per il tiro alla fune!</p>
+      <div>
+        <p>Aggiungi un giocatore:</p>
+        <form class="add-form">
+          <input type="text" name="name" placeholder="Inserisci il nome">
+          <input type="number" name="weight" placeholder="Inserisci il peso">
+          <button type="submit" @click=${this.addMember} class="full-row">Aggiungi</button>
+        </form>
+      </div>
+      <div class="member">${this.team1.length > 0 ? this.buildTeamMemberTable(this.team1, true) : html`<i>Aggiungi qualcuno</i>`}</div>
 
-    <button type="submit" class="btn btn-primary" @click=${this.calculate}>Calcola</button>
+      <button type="submit" style="width:100%" @click=${this.calculate}>Calcola</button>
+      <div class="member non-deletable">${this.solution.length > 0 ? this.buildTeamMemberTable(this.solution, false) : html`<i>Calcola</i>`}</div>
+    </div>
     `;
   }
 
-  addPlayer() {
-    const newMember = this.input.value;
-    if (!this.team1.find((element) => element === newMember)) {
-      this.team1 = [...this.team1,newMember];
-      console.log("Called with member: " + newMember);
+  private buildTeamMemberTable(memberList: TeamMember[], deletable: boolean) {
+    let team1Joined = html``;
+    memberList
+      .map(member => html`<div class="member-row bold">${member.name}</div>
+      <div class="member-row">${member.weight} Kg</div>
+      ${deletable ? html`<div class="member-row">
+        <span class="clickable" @click=${() => { this.removeMember(member.name); }}>X</span>
+      </div>` : null}
+      `)
+      .forEach(slice => team1Joined = html`${team1Joined}${slice}`);
+    return team1Joined;
+  }
+
+  protected firstUpdated(): void {
+    const memberListString = localStorage.getItem("memberList");
+    if (!!memberListString && memberListString.length > 0) {
+      const parsedMembers: TeamMember[] = JSON.parse(memberListString);
+      if (!!parsedMembers && parsedMembers.length > 0)
+        this.team1 = parsedMembers;
     }
   }
 
-  calculate() {
-    const listaNumeri: number[] = [1, 2, 3, 4, 5];
-    const m: number = 3;
-    const y: number = 9;
-
-    const risultato: number[] = this.trovaSottolistaProssimaSomma(listaNumeri, m, y);
-    console.log("Sottolista con somma più vicina a", y + ":", risultato);
+  private removeMember(memberName: string): void {
+    this.team1 = this.team1.filter(member => member.name !== memberName);
+    localStorage.setItem("memberList", JSON.stringify(this.team1));
   }
 
-  private trovaSottolistaProssimaSomma(lista: number[], m: number, y: number): number[] {
-    let miglioreSomma = 0;
-    let miglioreSottolista: number[] = [];
+  private addMember(event: Event) {
+    console.log("Adding...");
+    event.preventDefault();
+    const newMemberName: Element | RadioNodeList = this.$form.elements.namedItem("name");
+    const newMemberWeight: Element | RadioNodeList = this.$form.elements.namedItem("weight");
 
-    function combinazioni(arr: number[], n: number, callback: (combo: number[]) => void, combo: number[] = [], startIndex: number = 0): void {
+    if ("value" in newMemberName
+      && !this.team1.find((element) => element.name === newMemberName.value)
+      && "value" in newMemberWeight
+      && newMemberName.value.length > 0) {
+      this.team1 = [...this.team1, { name: newMemberName.value, weight: +newMemberWeight.value }];
+
+      localStorage.setItem("memberList", JSON.stringify(this.team1));
+    }
+  }
+
+  private calculate() {
+    const m: number = 3;
+    const y: number = 45;
+
+    this.solution = this.trovaSottolistaProssimaSomma(this.team1, m, y);
+  }
+
+  private trovaSottolistaProssimaSomma(lista: TeamMember[], m: number, y: number): TeamMember[] {
+    let miglioreSomma = 0;
+    let miglioreSottolista: TeamMember[] = [];
+
+    function combinazioni(arr: TeamMember[], n: number, callback: (combo: TeamMember[]) => void, combo: TeamMember[] = [], startIndex: number = 0): void {
       if (combo.length === n) {
         callback(combo);
         return;
@@ -63,7 +148,7 @@ export class MyCore extends LitElement {
     }
 
     combinazioni(lista, m, (combinazione) => {
-      const sommaCombinazione = combinazione.reduce((acc, val) => acc + val, 0);
+      const sommaCombinazione = combinazione.reduce((acc, val) => acc + val.weight, 0);
       if (Math.abs(sommaCombinazione - y) < Math.abs(miglioreSomma - y)) {
         miglioreSomma = sommaCombinazione;
         miglioreSottolista = [...combinazione];
